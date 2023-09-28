e4f89982c5c80a311b7a49b634ab519b97c69638
programmarshakil <shakljoka103@gmail.com>
2023-09-29 02:09:44 +0600

    migration updated

--- database/migrations/2023_09_28_165915_create_front_short_urls_table.php
+++ database/migrations/2023_09_28_165915_create_front_short_urls_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('original_url');
             $table->string('short_url')->unique();
             $table->integer('click_count')->default(0);
-            $table->timestamps();
+            $table->timestamps(); 
 
         });
     }
